7485abc805275e66823dc1d10913293bedde78d0
karthikHoney <jacksparowkarthick@gmail.com>
2023-12-22 17:49:24 +0530

    popular-books

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import {Route,Redirect,Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import React, { Component } from 'react';
+import Home from "./components/Home";
+import BooksList from './components/BooksList';
+import BookDetails from './components/BookDetails';
+import Cart from './components/Cart';
+import CheckOut from './components/CheckOut';
+import NotFound from './components/NotFound';
+
+import './App.css';
+
+class App extends Component{
+  render(){
+    return(
+     
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/books" component={BooksList}/>
+        <Route exact path="/books/:id" component={BookDetails}/>
+        <Route exact path="/cart" component={Cart}/>
+        <Route exact path="/checkout" component={CheckOut}/>
+        <Route exact path="/not-found" component={NotFound}/>
+        <Redirect to="/not-found"/>
+      </Switch>
+     
+    )
+  }
+}
+
+
+export default App;
